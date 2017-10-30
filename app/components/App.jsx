39429029943b04d83bcf8f1547e13dbe2915f4fe
Phanor Coll <phanorcoll@gmail.com>
2017-10-30 16:15:41 -0400

    video detail component
    added the basic structure and styles for the video detail.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,12 @@
 import React,{ Component } from 'react';
 import SearchBar from './SearchBar.jsx';
-
+import VideoDetail from './VideoDetail.jsx';
 class App extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <SearchBar />
-                <div className="content-fluid">
-                <h1>content - { this.props.text }</h1>
-                    </div>
+                <VideoDetail />
                 
             </div>
         );
