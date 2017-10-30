90155ee6e600df231eb56ca0cba4ddf7f6bb5b1c
Phanor Coll <phanorcoll@gmail.com>
2017-10-30 15:02:03 -0400

    added the searchbar to the main component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,16 @@
 import React,{ Component } from 'react';
+import SearchBar from './SearchBar.jsx';
 
 class App extends Component {
     render() {
         return (
-            <div><h1>Hello World - { this.props.text }</h1></div>
+            <div className="container-fluid">
+                <SearchBar />
+                <div className="content-fluid">
+                <h1>content - { this.props.text }</h1>
+                    </div>
+                
+            </div>
         );
     }
 }
