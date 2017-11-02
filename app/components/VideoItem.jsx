cd00f0a0bec3ba35d40c3e65cfaf83c011e4add9
Phanor Coll <phanorcoll@gmail.com>
2017-11-02 10:20:59 -0400

    added the icons to the UI

--- app/components/VideoItem.jsx
+++ app/components/VideoItem.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import PlayIcon from '../images/play-icon.png';
+import ChannelIcon from '../images/channel-icon.png';
 
 export default class VideoItem extends Component {
     constructor(props) {
@@ -11,17 +13,17 @@ export default class VideoItem extends Component {
         let thumbnail =this.props.video.snippet.thumbnails.high.url
         let channel =this.props.video.snippet.channelTitle
         return (
-            <div className="col">
+            <div className="col-md-4">
                 <div className="video-thumb-wrapper">
                     <figure>
-                        <img src={thumbnail} width="257px" height="424px" alt="" />
-                        <img src="images/play-icon.png" className="play" alt="" />
+                        <img src={thumbnail} className="thumbnail" width="257px" height="424px" alt="" />
+                        <img src={PlayIcon} className="play" alt="" />
                     </figure>
                     <div className="video-title">
                         {title}
                         </div>
                     <div className="video-channel">
-                        <img src="images/channel-icon.png" alt="" />
+                        <img src={ChannelIcon} alt="" />
                         <div className="channel-name">{channel}</div>
                     </div>
                 </div>
